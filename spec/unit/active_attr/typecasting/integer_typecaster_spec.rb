ad04c1954bd144eaab06162bf046983c26762648
Raz <raz.itzhakian@fiverr.com>
2013-12-02 16:49:47 +0200

    [FIX] Integer blank typecasting should be nil

--- spec/unit/active_attr/typecasting/integer_typecaster_spec.rb
+++ spec/unit/active_attr/typecasting/integer_typecaster_spec.rb
@@ -4,32 +4,34 @@ require "active_attr/typecasting/integer_typecaster"
 module ActiveAttr
   module Typecasting
     describe IntegerTypecaster do
-      subject(:typecaster) { described_class.new }
-
       describe "#call" do
         it "returns the original integer for a FixNum" do
           value = 2
-          typecaster.call(value).should equal value
+          subject.call(value).should equal value
         end
 
-        it "casts nil to 0" do
-          typecaster.call(nil).should eql 0
+        it "should not cast nil" do
+          subject.call(nil).should eql nil
+        end
+
+        it "should not cast blank" do
+          subject.call("").should eql nil
         end
 
         it "returns the integer version of a String" do
-          typecaster.call("2").should eql 2
+          subject.call("2").should eql 2
         end
 
         it "returns nil for an object that does not respond to #to_i" do
-          typecaster.call(Object.new).should be_nil
+          subject.call(Object.new).should be_nil
         end
 
         it "returns nil for Float::INFINITY" do
-          typecaster.call(1.0 / 0.0).should be_nil
+          subject.call(1.0 / 0.0).should be_nil
         end
 
         it "returns nil for Float::NAN" do
-          typecaster.call(0.0 / 0.0).should be_nil
+          subject.call(0.0 / 0.0).should be_nil
         end
       end
     end
